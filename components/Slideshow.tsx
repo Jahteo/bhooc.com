a3161eec11b89f0826a868a2cc9012e88ae924e5
Josh Glantz-Hucks <jahteo@gmail.com>
2021-07-01 16:17:38 -0700

    Slideshow style fixes. Much prettier. Design feedback for the win!

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -20,32 +20,30 @@ const Slideshow = ({imgSlides}
         {imgSlides.map(({url, header, text}) => {
           return (
             <div className="each-slide" key={url}>
-              <div style={{
-                backgroundColor: "grey",
-                backgroundImage: `url(${url})`,
-                padding: "0 20%",
-              }}>
-                <div style={{
-                  boxShadow: "inset 0px 1px 0px 0px #ffffff",
-                  background: "linear-gradient(to bottom, #ededed 5%, #dfdfdf 100%)",
-                  borderRadius: "6px",
-                  border: "1px solid #dcdcdc",
-                  padding: "10px 24px",
-                  // text-decoration:none,
-                  textShadow:"0px 1px 0px #ffffff",
-                  backgroundColor: "#fff",
-                  opacity: .9,
-                  lineHeight: "normal",
+              <div
+                id="banner"
+                style={{
+                  backgroundColor: "grey",
+                  backgroundImage: `url(${url})`,
+                  // padding: "0 20%",
+                  paddingBottom: "0",
                 }}>
-                  <h2 className="centered">
-                    <strong>{header}</strong>
-                  </h2>
-                  <p className="centered">
-                    <strong>{text}</strong>
-                  </p>
+                <div className="container"
+                  style={{
+                    width: "100%",
+                    color: "#000000",
+                    // background: "#6B6C6F",
+                    background: "#fff",
+                    backgroundColor: "#fff",
+                    padding: "10px 24px",
+                    // // textDecoration:none,
+                    opacity: .8,
+                    lineHeight: "normal",
+                  }}>
+                  <h2 style={{color: "#393838"}}>{header}</h2>
+                  <h3 style={{color: "#393838"}}>{text}</h3>
                   {/* <ul
                     className="actions"
-                    style={{textAlign: "center"}}
                   >
                     <li><a href="#" className="button icon solid">Continue Reading</a></li>
                   </ul> */}
